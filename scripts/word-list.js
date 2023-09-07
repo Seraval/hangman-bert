2ff9fd7b5fe24c2fb52bfaa1b940d0b74845dd6e
Gilbert <gilbert.lavares@apntelecom.com>
2023-09-07 16:37:20 +0800

    hangman game

---
+++ scripts/word-list.js
@@ -0,0 +1,161 @@
+const wordList = [
+    {
+        word: "elephant",
+        hint: "A large, gray mammal with a long trunk."
+    },
+    {
+        word: "sunflower",
+        hint: "A bright and cheerful flower that follows the sun."
+    },
+    {
+        word: "chocolate",
+        hint: "A sweet treat made from cocoa beans."
+    },
+    {
+        word: "giraffe",
+        hint: "The tallest land animal with a long neck."
+    },
+    {
+        word: "ocean",
+        hint: "A vast body of saltwater covering much of the Earth's surface."
+    },
+    {
+        word: "butterfly",
+        hint: "A flying insect with colorful wings."
+    },
+    {
+        word: "mountain",
+        hint: "A large landform that rises prominently above its surroundings."
+    },
+    {
+        word: "pizza",
+        hint: "A popular Italian dish with various toppings on a dough base."
+    },
+    {
+        word: "cactus",
+        hint: "A desert plant known for its spiky stems."
+    },
+    {
+        word: "fireworks",
+        hint: "Colorful explosions in the sky, often seen on special occasions."
+    },
+    {
+        word: "dolphin",
+        hint: "A marine mammal known for its intelligence and playful behavior."
+    },
+    {
+        word: "library",
+        hint: "A place with a collection of books for reading and studying."
+    },
+    {
+        word: "moonlight",
+        hint: "The soft, silvery light emitted by the moon."
+    },
+    {
+        word: "soccer",
+        hint: "A popular sport played with a ball and two teams on a rectangular field."
+    },
+    {
+        word: "penguin",
+        hint: "A flightless bird native to the Southern Hemisphere."
+    },
+    {
+        word: "keyboard",
+        hint: "A device with keys used to input text and commands into a computer."
+    },
+    {
+        word: "guitar",
+        hint: "A musical instrument with strings that is played by strumming or plucking."
+    },
+    {
+        word: "unicorn",
+        hint: "A mythical horse-like creature with a single horn on its forehead."
+    },
+    {
+        word: "beach",
+        hint: "A sandy shore by the sea, often a popular vacation spot."
+    },
+    {
+        word: "tiger",
+        hint: "A large, striped feline known for its strength and agility."
+    },    {
+        word: "butter",
+        hint: "A dairy product often used for cooking and spreading on bread."
+    },
+    {
+        word: "river",
+        hint: "A natural flowing watercourse, typically with banks."
+    },
+    {
+        word: "flower",
+        hint: "A colorful and fragrant blossom of a plant."
+    },
+    {
+        word: "tree",
+        hint: "A tall, woody plant with branches and leaves."
+    },
+    {
+        word: "oxygen",
+        hint: "A gas that is essential for human and animal respiration."
+    },
+    {
+        word: "mountain",
+        hint: "A high and rugged landmass with steep sides."
+    },
+    {
+        word: "jazz",
+        hint: "A genre of music characterized by improvisation and syncopation."
+    },
+    {
+        word: "pencil",
+        hint: "A writing instrument typically consisting of a thin cylindrical graphite core."
+    },
+    {
+        word: "rainbow",
+        hint: "A meteorological phenomenon with colors in the sky."
+    },
+    {
+        word: "moon",
+        hint: "Earth's natural satellite that orbits around it."
+    },
+    {
+        word: "phone",
+        hint: "A device used for voice communication."
+    },
+    {
+        word: "computer",
+        hint: "An electronic device for processing and storing data."
+    },
+    {
+        word: "robot",
+        hint: "A machine capable of carrying out tasks autonomously or by remote control."
+    },
+    {
+        word: "planet",
+        hint: "A celestial body that orbits a star."
+    },
+    {
+        word: "keyboard",
+        hint: "A set of input keys for typing on a computer or musical instrument."
+    },
+    {
+        word: "garden",
+        hint: "An outdoor area cultivated with plants, flowers, or vegetables."
+    },
+    {
+        word: "coffee",
+        hint: "A popular beverage made from roasted coffee beans."
+    },
+    {
+        word: "shark",
+        hint: "A large predatory fish found in oceans."
+    },
+    {
+        word: "island",
+        hint: "A piece of land surrounded by water."
+    },
+    {
+        word: "cloud",
+        hint: "A visible mass of water droplets or ice crystals in the sky."
+    }
+];
